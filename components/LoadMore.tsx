372665293c98fba461b818eaac8506453a667e4d
Abdohm-coder <hadjmedabdo@gmail.com>
2023-12-07 13:09:09 +0100

    feat infinite scroll

---
+++ components/LoadMore.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import { useInView } from "react-intersection-observer";
+import { fetchAnime } from "@/app/action";
+import AnimeCard, { AnimeProp } from "./AnimeCard";
+
+let page = 2;
+
+function LoadMore() {
+  const { ref, inView } = useInView();
+  const [data, setData] = useState<AnimeProp[]>([]);
+
+  useEffect(() => {
+    if (inView)
+      fetchAnime(page).then((res) => {
+        setData((state) => [...state, ...res]);
+        page++;
+      });
+  }, [inView]);
+  return (
+    <>
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data.map((item, index) => (
+          <AnimeCard key={item.id} anime={item} index={index} />
+        ))}
+      </section>
+      <section className="flex justify-center items-center w-full">
+        <div ref={ref}>
+          <Image
+            src="./spinner.svg"
+            alt="spinner"
+            width={56}
+            height={56}
+            className="object-contain"
+          />
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default LoadMore;
